3a952da4c173e5dbb1109606521461ec2219e5ae
Odun Freeman <bornduringwar@gmail.com>
2022-06-13 17:26:56 -0400

    .gitignore

--- config/db.js
+++ config/db.js
@@ -3,5 +3,5 @@ const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI);
   console.log(`MongoDB Connected ${conn.connection.host}`.cyan.underline.bold);
 };
-
+sdf;
 module.exports = connectDB;
